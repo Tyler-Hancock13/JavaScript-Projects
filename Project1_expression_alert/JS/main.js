7a3e62a1ffe8dad54a7c7f2bcbad78724b9c12a5
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-03-16 00:05:32 -0300

    Complete Assignment Submission #1

---
+++ Project1_expression_alert/JS/main.js
@@ -0,0 +1,34 @@
+window.alert("Hello World!"); //Print Hello World!
+document.write("This is some text."); //Prints text
+
+var A = "This is a string";
+document.write(A);
+
+//Assignment 7
+var alertText = "This is the alert text."; //Sets the alert text
+
+window.alert(alertText); //Displays the alertText value in an alert window
+
+//Assignment 8
+document.write("Come on, let\'s go"); 
+
+//Assignment 9
+document.write("Using concatenation, " + "this is sentence one, " + "and this is sentence two."); //Displays a sentence that uses string concatenation
+
+//Challenge #1
+var concatValue = "This is a " + "concatenated string";
+document.write(concatValue);
+
+//Assignment 10
+var vehicleOne = "Car", vehicleTwo = "Truck", vehicleThree = "Bus"; //Sets multiple variables
+document.write(vehicleTwo);
+
+//Assignment 11
+vehicleOne = vehicleOne.fontcolor("Green"); //Sets vehicleOne text to green
+vehicleTwo = vehicleTwo.fontcolor("Red"); //Sets vehicleTwo text to red
+vehicleThree = vehicleThree.fontcolor("Yellow"); //Sets vehicleThree text to yellow
+document.write(vehicleOne, vehicleTwo, vehicleThree); //Displays vehicleOne, vehicleTwo, vehicleThree in the browser window
+
+//Assignment 12
+result = 3 - 2; //This is an expression
+document.write(result); //This is a statement
